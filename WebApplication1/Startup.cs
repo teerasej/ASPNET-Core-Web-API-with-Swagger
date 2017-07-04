e3ef68a30c265713061ee9b645d844f49aed9393
Teerasej Jiraphatchandej <teerasej@gmail.com>
2017-07-04 20:42:51 +0700

    Add reference

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApplication1
 {
